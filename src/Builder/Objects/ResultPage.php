7f4d076ae1497a9709f52293eda20db0179d1e1d
philipp-riddle <53085316+philipp-riddle@users.noreply.github.com>
2024-09-25 12:08:04 +0200

    add: marginTop/marginBottom property to every result page block

--- src/Builder/Objects/ResultPage.php
+++ src/Builder/Objects/ResultPage.php
@@ -10,34 +10,40 @@ class ResultPage
 {
     private array $blocks = [];
 
-    public function addTextBlock(string $text): self
+    public function addTextBlock(string $text, int $marginTop = 0, int $marginBottom = 0): self
     {
         $this->blocks[] = [
             'type' => 'Text',
             'text' => $text,
+            'marginTop' => $marginTop,
+            'marginBottom' => $marginBottom,
         ];
 
         return $this;
     }
 
-    public function addButton(string $label, string $url, bool $openInNewTab = false): self
+    public function addButton(string $label, string $url, bool $openInNewTab = false, int $marginTop = 0, int $marginBottom = 0): self
     {
         $this->blocks[] = [
             'type' => 'Button',
             'label' => $label,
             'url' => $url,
             'isOpenInNewTabEnabled' => $openInNewTab,
+            'marginTop' => $marginTop,
+            'marginBottom' => $marginBottom,
         ];
 
         return $this;
     }
 
-    public function addSocialShare(string $label, string $shareMessage = '', string $shareDescription = '', ?string $shareRedirectUrl = null, array $networks = ['facebook', 'whatsapp', 'twitter']): self
+    public function addSocialShare(string $label, string $shareMessage = '', string $shareDescription = '', ?string $shareRedirectUrl = null, array $networks = ['facebook', 'whatsapp', 'twitter'], int $marginTop = 0, int $marginBottom = 0): self
     {
         $shareBlock = [
             'type' => 'Share',
             'label' => $label,
             'networks' => $networks,
+            'marginTop' => $marginTop,
+            'marginBottom' => $marginBottom,
         ];
 
         if ('' !== $shareMessage) {
@@ -57,7 +63,7 @@ class ResultPage
         return $this;
     }
 
-    public function addAnsweredBlocks(bool $areTotalVotesVisible = true, bool $isPercentageVisible = true, bool $isVotesNumberVisible = true, bool $areAnswerImagesHidden = false, bool $areMainImagesHidden = false): self
+    public function addAnsweredBlocks(bool $areTotalVotesVisible = true, bool $isPercentageVisible = true, bool $isVotesNumberVisible = true, bool $areAnswerImagesHidden = false, bool $areMainImagesHidden = false, int $marginTop = 0, int $marginBottom = 0): self
     {
         $this->blocks[] = [
             'type' => 'AnsweredBlocks',
@@ -66,26 +72,32 @@ class ResultPage
             'isVotesNumberVisible' => $isVotesNumberVisible,
             'areAnswerImagesHidden' => $areAnswerImagesHidden,
             'areMainImagesHidden' => $areMainImagesHidden,
+            'marginTop' => $marginTop,
+            'marginBottom' => $marginBottom,
         ];
 
         return $this;
     }
 
-    public function addMedia(string $mediaUrl): self
+    public function addMedia(string $mediaUrl, int $marginTop = 0, int $marginBottom = 0): self
     {
         $this->blocks[] = [
             'type' => 'Media',
             'media' => $mediaUrl,
+            'marginTop' => $marginTop,
+            'marginBottom' => $marginBottom,
         ];
 
         return $this;
     }
 
-    public function addLeaderboard(string $leaderboardUUID): self
+    public function addLeaderboard(string $leaderboardUUID, int $marginTop = 0, int $marginBottom = 0): self
     {
         $this->blocks[] = [
             'type' => 'Leaderboard',
             'leaderboard' => $leaderboardUUID,
+            'marginTop' => $marginTop,
+            'marginBottom' => $marginBottom,
         ];
 
         return $this;
